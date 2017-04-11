8424786e24bf45c0b95c756f02d7011270167e26
ashleyyy <ashleyfisher@gmail.com>
2017-04-11 15:19:45 -0700

    add slack token

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'httparty'
 require 'json'
 
 post '/gateway' do
-  # return if params[:token] != ENV['SLACK_TOKEN']
+  return if params[:token] != ENV['SLACK_TOKEN']
   
   message = params[:text].gsub(params[:trigger_word], '').strip
 

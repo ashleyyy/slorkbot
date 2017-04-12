b69b6dd9e82af4114a07f720954262c5c36ac333
ashleyyy <ashleyfisher@gmail.com>
2017-04-11 20:49:45 -0700

    change /anonymize

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ def respond_message message
 end
 
 get '/anonymize' do
-  content_type :json
-  {:text => params[:text], :response_type => "in_channel"}.to_json
+  postback params[:text], params[:channel_id]
+  status 200
 end
  
 def postback message, channel

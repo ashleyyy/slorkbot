4b9464a4cdb29707eb42f30a6e434beb017df1d3
ashleyyy <ashleyfisher@gmail.com>
2017-04-11 21:28:45 -0700

    sorry

--- app.rb
+++ app.rb
@@ -30,12 +30,17 @@ def respond_message message
 end
 
 get '/anonymize' do
-  respond_message params[:text]
-  # , params[:channel_id]
-  # status 200
+  postback params[:text], params[:channel_id]
+  status 200
 end
  
-# def postback message, channel
-#     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-#     HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "John Doe", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
-# end
+def postback message, channel
+    # slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    url = ENV['SLACK_WEBHOOK_URL'] + "&channel=%23test"
+    msg = params[:text]
+    HTTParty.post(url, body: {"text":"#{msg}"}.to_json)
+
+    # HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "John Doe", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+
+    # curl --data "Hello from Slackbot" $'https://horacephair.slack.com/services/hooks/slackbot?token=lzGQgtMVo2e6cb9ewpWmvqET&channel=%23general'
+end

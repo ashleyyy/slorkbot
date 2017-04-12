1dbb9524b6bc72b4403540530df9702cb6e2cf01
ashleyyy <ashleyfisher@gmail.com>
2017-04-11 17:17:13 -0700

    add anonymize path

--- app.rb
+++ app.rb
@@ -28,3 +28,13 @@ def respond_message message
   content_type :json
   {:text => message}.to_json
 end
+
+get '/anonymize' do
+  postback params[:text], params[:channel_id]
+  status 200
+end
+ 
+def postback message, channel
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "John Doe", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+end

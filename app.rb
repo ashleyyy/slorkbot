d8e81193df42e9a2c0a26e8240538e156e0bcfbb
ashleyyy <ashleyfisher@gmail.com>
2017-04-12 12:02:28 -0700

    rollback

--- app.rb
+++ app.rb
@@ -7,22 +7,19 @@ post '/gateway' do
 
   message = params[:text].gsub(params[:trigger_word], '').strip
 
-  action = message.split.first.downcase!
+  action = message.split.first
   term = message.split(' ')[1..-1].join(' ')
 
-  compliments = ["you are soooooo good lookin'"]
-    # , "your teeth are lookin' shiny today", "I admire your courage", "nobody does it better than you", "you are kind to animals", "you are so beautiful that you know what I mean", "you’re to be a key? Because I can bear your toot?", "you look like a thing and I love you", "you are so beautiful that you make me feel better to see you"]
+  compliments = ["you are soooooo good lookin'", "your teeth are lookin' shiny today", "I admire your courage", "nobody does it better than you", "you are kind to animals", "you are so beautiful that you know what I mean", "you’re to be a key? Because I can bear your toot?", "You look like a thing and I love you", "you are so beautiful that you make me feel better to see you"]
 
   case action
   # when 'ashley'
   #   respond_message "Ashley, you are soooooo good lookin'"
-  when 'i'
+  when 'I'
     respond_message params[:user_name] + ", " + compliments[rand(compliments.length)]
   when 'define'
     term = term.gsub(" ", "+").gsub("\"", "")
     respond_message "http://lmgtfy.com/?q=#{term}"
-  when 'add'
-    add_message term
   else 
     if term == "needs love"
       respond_message action + ", " + compliments[rand(compliments.length)]
@@ -33,14 +30,7 @@ end
 def respond_message message
   content_type :json
   {:text => message}.to_json
-  #TODO include ethereal response to successful message add
 end
-
-def add_message message
-  compliments.push(message) 
-  respond_message "Thanks, I'll pass that on"
-end
-
 
 get '/anonymize' do
   postback params[:text], params[:channel_id]

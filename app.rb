5e7cf50363ade0d6c5791f2237282574d0c39c71
ashleyyy <ashleyfisher@gmail.com>
2017-04-18 18:23:01 -0700

    all case

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ post '/gateway' do
   case action.downcase
   when 'listallcomplimentsplease'
     @models = Model.all 
-    @models.each { |m| respond_message m.compliment }
+    @models.each { |m| respond_message m }
   when 'ashley'
     respond_message "Ashley is my creator! She needs for nothing, except maybe a full time jorb"
   when 'nuke'
